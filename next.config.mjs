b933607f539862834fa2a746290acee9fab36244
KcPele <fidekg123@gmail.com>
2024-02-15 23:45:12 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/comics",
+        destination: "https://cosmiccowboys.cloud",
+        permanent: false,
+      },
+      {
+        source: "/cloud",
+        destination:
+          "https://www.pinata.cloud/blog/how-to-make-a-frame-on-farcaster-using-ipfs",
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
